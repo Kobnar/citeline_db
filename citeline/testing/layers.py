0212b05cf3e63b1772f7e710a24f86d5f1f52163
Kobnar <konrad.rk.ludwig@gmail.com>
2016-06-16 02:06:18 -0700

    Add data.mongo.utils

--- citeline/testing/layers.py
+++ citeline/testing/layers.py
@@ -1,4 +1,29 @@
+import os
+import mongoengine
+
+
 class UnitTestLayer(object):
     """
     The base test layer for `citeline`.
     """
+
+
+class MongoIntegrationTestLayer(UnitTestLayer):
+    """
+    A test layer that establishes an active connection with the test database.
+    """
+
+    _IP = os.environ.get('MONGO_TEST_IP', 'http://127.0.0.1/')
+    _DB = os.environ.get('MONGO_TEST_DB', 'test')
+    _USER = os.environ.get('MONGO_TEST_USER', 'test')
+    _PASSWORD = os.environ.get('MONGO_TEST_PASSWORD', 'test')
+
+    @classmethod
+    def setUp(cls):
+        db = mongoengine.connect(
+            cls._DB,
+            username=cls._USER,
+            password=cls._PASSWORD
+        )
+        db.drop_database(cls._DB)
+
