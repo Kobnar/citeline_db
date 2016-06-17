51ad01ffc70d27a6c3c320aa49bde98e22a66fd8
Kobnar <konrad.rk.ludwig@gmail.com>
2016-06-17 05:23:19 -0700

    Expose mongodb collection models directly in citeline.data package

--- citeline/data/__init__.py
+++ citeline/data/__init__.py
@@ -1,2 +1,8 @@
-from .mongo import citations, locale, organizations, people, sources, utils
+from .mongo.citations import Citation, TextCitation, BookCitation
+from .mongo.organizations import Organization, Publisher
+from .mongo.people import Person
+from .mongo.sources import Source, TextSource, BookSource
+
+from .mongo import utils
+
 from . import validators
